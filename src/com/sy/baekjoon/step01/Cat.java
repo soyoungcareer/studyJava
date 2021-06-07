225c2a89308a43a1d347c74e67013cec10de546e
soyoungcareer <soyoungcareer@gmail.com>
2021-06-07 17:55:22 +0900

    Coding Test Java Study
    Questions from baekjoon
    Step1 question 1 to 3

---
+++ src/com/sy/baekjoon/step01/Cat.java
@@ -0,0 +1,12 @@
+package com.sy.baekjoon.step01;
+
+public class Cat {
+
+	public static void main(String[] args) {
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+	}
+
+}
