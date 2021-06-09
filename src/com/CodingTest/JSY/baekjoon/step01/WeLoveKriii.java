1e55d35fba64e9b96cf7201d52462d8cea8f9766
soyoungcareer <soyoungcareer@gmail.com>
2021-06-09 15:05:00 +0900

    Baekjoon step1 question number 1 to 6
    Date 20210607-20210608
    Coding test study with Java.
    Questions from Baekjoon.

---
+++ src/com/CodingTest/JSY/baekjoon/step01/WeLoveKriii.java
@@ -0,0 +1,10 @@
+package com.CodingTest.JSY.baekjoon.step01;
+
+public class WeLoveKriii {
+
+	public static void main(String[] args) {
+		System.out.println("강한친구 대한육군");
+		System.out.println("강한친구 대한육군");
+	}
+
+}
