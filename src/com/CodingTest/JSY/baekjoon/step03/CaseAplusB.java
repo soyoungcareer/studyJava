be1f65bbf2ab92e701ab194b93a4a4e6158e206c
soyoungcareer <soyoungcareer@gmail.com>
2021-06-16 16:28:44 +0900

    Baekjoon step3 question number 6 to 8
    Date 20210616
    Coding test study with Java.
    Questions from Baekjoon.

---
+++ src/com/CodingTest/JSY/baekjoon/step03/CaseAplusB.java
@@ -0,0 +1,35 @@
+package com.CodingTest.JSY.baekjoon.step03;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class CaseAplusB {
+
+	public static void main(String[] args) throws IOException {
+		// 20210616
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int times = Integer.parseInt(br.readLine());
+		
+		for (int i = 1; i <= times; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			
+			int sum = a + b;
+			
+			bw.write("Case #" + i + ": ");
+			bw.append(String.valueOf(sum));
+			bw.newLine();
+		}
+		
+		bw.flush();
+		br.close();
+		bw.close();
+	}
+}
---
+++ src/com/CodingTest/JSY/baekjoon/step03/ReverseNStamp.java
@@ -0,0 +1,27 @@
+package com.CodingTest.JSY.baekjoon.step03;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class ReverseNStamp {
+
+	public static void main(String[] args) throws IOException {
+		// 20210616
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int num = Integer.parseInt(br.readLine());
+		
+		for (int i = num; i > 0; i--) {
+			bw.write(String.valueOf(i));
+			bw.newLine();
+		}
+		
+		bw.flush();
+		br.close();
+		bw.close();
+	}
+}
