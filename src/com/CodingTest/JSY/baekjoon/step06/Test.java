d424d7890544a4c80754b4f59371202432d5a0b8
soyoungcareer <soyoungcareer@gmail.com>
2021-06-23 20:06:56 +0900

    Baekjoon step5 question number 7 and step6 number 1 to 2
    Date 20210623
    Coding test study with Java.
    Questions from Baekjoon.

---
+++ src/com/CodingTest/JSY/baekjoon/step06/Test.java
@@ -0,0 +1,16 @@
+package com.CodingTest.JSY.baekjoon.step06;
+
+public class Test {
+		// 20210623 - 15596번
+//		Java: long sum(int[] a); (클래스 이름: Test)
+//		a: 합을 구해야 하는 정수 n개가 저장되어 있는 배열 (0 ≤ a[i] ≤ 1,000,000, 1 ≤ n ≤ 3,000,000)
+//		리턴값: a에 포함되어 있는 정수 n개의 합
+		
+		long sum(int[] a) {
+			long ans = 0;
+			for (int i = 0; i < a.length; i++) {
+				ans += a[i];
+			}
+			return ans;
+		}
+}
